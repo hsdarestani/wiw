80d2476b099abf6bc38f5d1d970393ebc5998d43
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-09-18 00:03:49 +0330

    feat: add mobile manager shift creation

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -49,6 +49,7 @@ export default function TabsLayout() {
       />
       <Tabs.Screen name="notifications" options={{ href: null }} />
       <Tabs.Screen name="password" options={{ href: null }} />
+      <Tabs.Screen name="manage-shift" options={{ href: null }} />
       <Tabs.Screen
         name="more"
         options={{
